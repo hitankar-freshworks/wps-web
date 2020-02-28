dca2f65c5ef4c31049e78bfea601a9f8039986cc
Andrea Williams <andrea.williams@gov.bc.ca>
2020-02-27 17:01:11 -0800

    Change documentation from TextField to Card

--- src/features/fwiCalculator/components/CalculationDocumentation.tsx
+++ src/features/fwiCalculator/components/CalculationDocumentation.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { TextField } from '@material-ui/core'
+import { Card, CardContent, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles({
   root: {
     marginTop: 15,
+    marginBottom: 15,
     width: 600
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: '#000000'
+  },
+  bodyText: {
+    color: '#333333',
+    fontSize: 14
   }
 })
 
@@ -13,22 +23,20 @@ export const CalculationDocumentation = () => {
   const classes = useStyles()
 
   return (
-    <TextField
-      id="documentation-textfield"
-      className={classes.root}
-      data-testid="documentation-textfield"
-      multiline
-      disabled
-      variant="outlined"
-      InputProps={{
-        readOnly: true
-      }}
-      label="How Are These Values Calculated?"
-      defaultValue="Daily weather data for the fire season (May 1 - August 31) for the years 2010-2019 is collected for each of the selected weather stations. 
-      The value reported for each of the FFMC, BUI, and ISI indexes represents the 90th percentile of those index values for each individual weather station for the 
-      selected time range. When more than one weather station is selected, the mean for the indexes of each 90th percentile is also calculated.
-      
-      The 90th percentile indicates the 1080th largest value for each daily index value at the weather station. (30 days * 4 months * 10 years * 0.9 = 1080)"
-    />
+    <Card className={classes.root} id="documentation-card" data-testid="documentation-card">
+      <CardContent>
+        <Typography className={classes.title}>
+          How Are These Values Calculated?
+        </Typography>
+        <Typography className={classes.bodyText}>
+          Daily weather data (collected at noon local time) for the core fire seasons in the selected time range is collected for each of the selected weather stations. 
+          <br/><br/>
+          The value reported for each of the FFMC, BUI, and ISI indexes represents the 90th percentile of those index values for each individual weather station over the time range. 
+          When more than one weather station is selected, the mean for the indexes of each 90th percentile is also calculated. 
+          <br/><br/>
+          The 90th percentile indicates the 1080th largest value for each daily index value at the weather station (30 days * 4 months * 10 years * 0.9 = 1080).
+        </Typography>
+      </CardContent>
+    </Card>    
   )
 }

fdcf76b7be8c80c1f7c6103a3ad33db3fcb50870
Andrea Williams <andrea.williams@gov.bc.ca>
2020-03-03 15:22:27 -0800

    Fix linter-raised whitespace complaints

--- src/features/fwiCalculator/components/CalculationDocumentation.tsx
+++ src/features/fwiCalculator/components/CalculationDocumentation.tsx
@@ -33,14 +33,21 @@ export const CalculationDocumentation = () => {
           How Are These Values Calculated?
         </Typography>
         <Typography className={classes.bodyText}>
-          Daily weather data (collected at noon local time) for the core fire seasons in the selected time range is collected for each of the selected weather stations. 
-          <br/ >
-          <br/ >
-          The value reported for each of the FFMC, BUI, and ISI indexes represents the 90th percentile of those index values for each individual weather station over the time range. 
-          When more than one weather station is selected, the mean for the indexes of each 90th percentile is also calculated. 
-          <br/ >
-          <br/ >
-          The 90th percentile indicates the 1080th largest value for each daily index value at the weather station (30 days * 4 months * 10 years * 0.9 = 1080).
+          Daily weather data (collected at noon local time) for the core fire
+          seasons in the selected time range is collected for each of the
+          selected weather stations.
+          <br />
+          <br />
+          The value reported for each of the FFMC, BUI, and ISI indexes
+          represents the 90th percentile of those index values for each
+          individual weather station over the time range. When more than one
+          weather station is selected, the mean for the indexes of each 90th
+          percentile is also calculated.
+          <br />
+          <br />
+          The 90th percentile indicates the 1080th largest value for each daily
+          index value at the weather station (30 days * 4 months * 10 years *
+          0.9 = 1080).
         </Typography>
       </CardContent>
     </Card>

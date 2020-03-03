bde321a3f531d8b5e77738c33e734eac2e3fea02
Andrea Williams <andrea.williams@gov.bc.ca>
2020-03-03 14:42:09 -0800

    Calculation documentation exists as card instead of TextField

--- src/features/fwiCalculator/components/CalculationDocumentation.tsx
+++ src/features/fwiCalculator/components/CalculationDocumentation.tsx
@@ -23,20 +23,23 @@ export const CalculationDocumentation = () => {
   const classes = useStyles()
 
   return (
-    <Card 
-      className={classes.root} 
-      id="documentation-card" 
-      data-testid="documentation-card">
+    <Card
+      className={classes.root}
+      id="documentation-card"
+      data-testid="documentation-card"
+      >
       <CardContent>
         <Typography className={classes.title}>
           How Are These Values Calculated?
         </Typography>
         <Typography className={classes.bodyText}>
           Daily weather data (collected at noon local time) for the core fire seasons in the selected time range is collected for each of the selected weather stations. 
-          <br/><br/>
+          <br/>
+          <br/>
           The value reported for each of the FFMC, BUI, and ISI indexes represents the 90th percentile of those index values for each individual weather station over the time range. 
           When more than one weather station is selected, the mean for the indexes of each 90th percentile is also calculated. 
-          <br/><br/>
+          <br/>
+          <br/>
           The 90th percentile indicates the 1080th largest value for each daily index value at the weather station (30 days * 4 months * 10 years * 0.9 = 1080).
         </Typography>
       </CardContent>

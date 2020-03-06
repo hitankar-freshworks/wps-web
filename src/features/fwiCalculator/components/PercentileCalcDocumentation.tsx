5bd6b9397c99e9e22308f1a2a5f982c323a64203
Kyubin Han <kyubinhan11@gmail.com>
2020-03-06 15:53:27 -0800

    Wrap "th"s with <sup> tags

--- src/features/fwiCalculator/components/PercentileCalcDocumentation.tsx
+++ src/features/fwiCalculator/components/PercentileCalcDocumentation.tsx
@@ -37,16 +37,17 @@ export const PercentileCalcDocumentation = () => {
           </div>
           <div className={classes.bodyText}>
             The value reported for each of the FFMC, BUI, and ISI indexes
-            represents the 90th percentile of those index values for each
-            individual weather station over the time range. When more than one
-            weather station is selected, the mean for the indexes of each 90th
-            percentile is also calculated.
+            represents the 90<sup>th</sup> percentile of those index values for
+            each individual weather station over the time range. When more than
+            one weather station is selected, the mean for the indexes of each 90
+            <sup>th</sup> percentile is also calculated.
           </div>
           <div className={classes.bodyText}>
             For example, when looking at a weather station over 10 years for a
-            4-month long core fire season, the 90th percentile indicates the
-            1080th largest value for each daily index value at the weather
-            station (30 days * 4 months * 10 years * 0.9 = 1080).
+            4-month long core fire season, the 90<sup>th</sup> percentile
+            indicates the 1080<sup>th</sup> largest value for each daily index
+            value at the weather station (30 days * 4 months * 10 years * 0.9 =
+            1080).
           </div>
         </Typography>
       </CardContent>
